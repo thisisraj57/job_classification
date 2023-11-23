6246eb5b76341cfe5e65740c1df25b4bed2f77b4
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2023-11-24 03:09:37 +0530

    first commit

---
+++ htmlTemplates.py
@@ -0,0 +1,48 @@
+css = '''
+<style>
+body {
+    background-color: orange;
+    }
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://cdn.pixabay.com/photo/2023/05/01/13/17/ai-generated-7963061_1280.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://cdn.pixabay.com/photo/2015/10/31/12/00/question-1015308_1280.jpg">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
